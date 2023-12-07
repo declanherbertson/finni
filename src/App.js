ce1214c09e5df5e31e81c3209b6e4097f136cd01
declanherbertson <declanherbertson@gmail.com>
2023-12-06 17:46:55 -0800

    installing and testing material ui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import EditIcon  from '@mui/icons-material/Edit';
+import { Button } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" startIcon={<EditIcon />}>Hello World</Button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import { getPatients } from './patient';
 
+// Material UI font imports
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 // Test firebase connection
 getPatients().then(qs => {
